999ea3ec0d520975f53c7edc1e3fb321363ce231
harisejaz2206 <harisejaz2206@gmail.com>
2024-09-07 01:57:16 +0500

    setting up boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-
+import Home from "./pages/Home";
 
 function App() {
+  // const token = useSelector(selectAuthToken);
+
+  // useLayoutEffect(() => {
+  //   if (token) {
+  //     HttpService.setToken(token);
+  //     localStorage.setItem("token", token);
+  //     console.log("Token set globally");
+  //   }
+  // }, [token]);
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
       </Routes>
     </BrowserRouter>
   );
